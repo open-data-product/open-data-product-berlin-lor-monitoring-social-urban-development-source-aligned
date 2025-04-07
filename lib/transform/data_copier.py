741baa47734d6e0f831d9c48480eb07a729e8f77
Florian Schwanz <florian.schwanz@interoberlin.de>
2025-04-08 01:11:55 +0200

    chore: add data product manifest loader

--- lib/transform/data_copier.py
+++ lib/transform/data_copier.py
@@ -65,7 +65,10 @@ def get_results_file_name(subdir, source_file_name):
         return "berlin-lor-monitoring-social-urban-development-2017-00-1.xlsx"
     if source_file_name == "2-1-indexind_anteile_plr_mss2017.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2017-00-2-1.xlsx"
-    if source_file_name == "2-2-indexind_anteile_bzr_mss2019.xlsx" and subdir == "berlin-lor-monitoring-social-urban-development-2017-00":
+    if (
+        source_file_name == "2-2-indexind_anteile_bzr_mss2019.xlsx"
+        and subdir == "berlin-lor-monitoring-social-urban-development-2017-00"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2017-00-2-2.xlsx"
     if source_file_name == "2.3.indexind_anteile_bezirke_mss2017.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2017-00-2-3.xlsx"
@@ -81,7 +84,10 @@ def get_results_file_name(subdir, source_file_name):
         return "berlin-lor-monitoring-social-urban-development-2019-00-1.xlsx"
     if source_file_name == "2-1-indexind_anteile_plr_mss2019.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2019-00-2-1.xlsx"
-    if source_file_name == "2-2-indexind_anteile_bzr_mss2019.xlsx" and subdir == "berlin-lor-monitoring-social-urban-development-2019-00":
+    if (
+        source_file_name == "2-2-indexind_anteile_bzr_mss2019.xlsx"
+        and subdir == "berlin-lor-monitoring-social-urban-development-2019-00"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2019-00-2-2.xlsx"
     if source_file_name == "2-3-indexind_anteile_bezirke_mss2019.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2019-00-2-3.xlsx"
@@ -93,13 +99,25 @@ def get_results_file_name(subdir, source_file_name):
         return "berlin-lor-monitoring-social-urban-development-2019-00-4-2.xlsx"
     if source_file_name == "4.3.kontextind_anteile_bezirke_mss2019.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2019-00-4-3.xlsx"
-    if source_file_name == "tabelle_1_gesamtindex_soziale_ungleichheit_sdi_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_1_gesamtindex_soziale_ungleichheit_sdi_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-1.xlsx"
-    if source_file_name == "tabelle_2-1_index-indikatoren_anteilswerte_auf_planungsraum-ebene_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_2-1_index-indikatoren_anteilswerte_auf_planungsraum-ebene_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-2-1.xlsx"
-    if source_file_name == "tabelle_2-2_index-indikatoren_anteilswerte_auf_bezirksregionen-ebene_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_2-2_index-indikatoren_anteilswerte_auf_bezirksregionen-ebene_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-2-2.xlsx"
-    if source_file_name == "tabelle_2-3_index-indikatoren_auf_ebene_der_bezirke_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_2-3_index-indikatoren_auf_ebene_der_bezirke_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-2-3.xlsx"
     if source_file_name == "tabelle_3_index-indikatoren_z-werte_mss_2021.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2021-00-3.xlsx"
@@ -107,13 +125,25 @@ def get_results_file_name(subdir, source_file_name):
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-1.xlsx"
     if source_file_name == "tabelle_4-2_kontext-indikatoren_anteile_bzr_mss_2021.xlsx":
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-2.xlsx"
-    if source_file_name == "tabelle_4-3_kontext-indikatoren_anteile_bezirke_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_4-3_kontext-indikatoren_anteile_bezirke_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-3.xlsx"
-    if source_file_name == "tabelle_4-1-1_kontext-indikatoren_anteile_plr_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_4-1-1_kontext-indikatoren_anteile_plr_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-1-1.xlsx"
-    if source_file_name == "tabelle_4-2-1_kontext-indikatoren_anteile_bzr_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_4-2-1_kontext-indikatoren_anteile_bzr_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-2-1.xlsx"
-    if source_file_name == "tabelle_4-3-1_kontext-indikatoren_anteile_bezirke_mss_2021.xlsx":
+    if (
+        source_file_name
+        == "tabelle_4-3-1_kontext-indikatoren_anteile_bezirke_mss_2021.xlsx"
+    ):
         return "berlin-lor-monitoring-social-urban-development-2021-00-4-3-1.xlsx"
     else:
         return source_file_name
